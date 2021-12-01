adad01a58596556b4558dc430091504794e4f3af
Volodymyr Masliy <thebestvova@gmail.com>
2021-12-01 13:57:52 +0200

    day1, part2 simplier solution

--- day1/day1.go
+++ day1/day1.go
@@ -7,21 +7,11 @@ func MeasureIncrements(in []int) int {
 		return 0
 	}
 
-	windowsCount := len(in) - 2
 	count := 0
-	prevSum := 0
-	sum := 0
-	for w := 0; w < windowsCount; w++ {
-		for i := w; i < w+3; i++ {
-			sum += in[i]
-		}
-
-		if prevSum > 0 && prevSum < sum {
+	for i := 3; i < len(in); i++ {
+		if in[i-3] < in[i] {
 			count++
 		}
-
-		prevSum = sum
-		sum = 0
 	}
 
 	return count
