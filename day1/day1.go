0827957437a3d4eddaab16454d640c1928870027
Volodymyr Masliy <thebestvova@gmail.com>
2021-12-01 13:47:16 +0200

    part2: simple sum

--- day1/day1.go
+++ day1/day1.go
@@ -1,17 +1,27 @@
 package day1
 
+const minCountOfDigits = 4
+
 func MeasureIncrements(in []int) int {
-	if len(in) < 2 {
+	if len(in) < minCountOfDigits {
 		return 0
 	}
 
-	curr := in[0]
+	windowsCount := len(in) - 2
 	count := 0
-	for _, v := range in {
-		if v > curr {
+	prevSum := 0
+	sum := 0
+	for w := 0; w < windowsCount; w++ {
+		for i := w; i < w+3; i++ {
+			sum += in[i]
+		}
+
+		if prevSum > 0 && prevSum < sum {
 			count++
 		}
-		curr = v
+
+		prevSum = sum
+		sum = 0
 	}
 
 	return count
