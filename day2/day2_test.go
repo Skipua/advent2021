8cbf49eea133a667c5e08a518fd05be8e397c871
Volodymyr Masliy <thebestvova@gmail.com>
2021-12-02 12:41:09 +0200

    day2 part2

--- day2/day2_test.go
+++ day2/day2_test.go
@@ -17,8 +17,14 @@ func TestSubmarine_Steer(t *testing.T) {
 	}{
 		{
 			"example_from_site",
-			args{[]string{"forward 5", "down 5"}},
-			-25,
+			args{[]string{
+				"forward 5",
+				"down 5",
+				"forward 8",
+				"up 3",
+				"down 8",
+				"forward 2"}},
+			900,
 		},
 	}
 	for _, tt := range tests {
