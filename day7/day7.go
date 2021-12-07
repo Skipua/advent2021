323000c5297235d05dbbc3ac60474bc276b70936
Volodymyr Masliy <thebestvova@gmail.com>
2021-12-07 19:48:58 +0200

    day7 part2 simple solution again

--- day7/day7.go
+++ day7/day7.go
@@ -4,10 +4,10 @@ import "math"
 
 func AlignCrabs(crabPositions []int) int {
 	minFuel := math.MaxInt
-	for _, c1 := range crabPositions {
+	for _, initPos := range crabPositions {
 		fuel := 0
-		for _, c2 := range crabPositions {
-			fuel += int(math.Abs(float64(c1) - float64(c2)))
+		for _, targetPos := range crabPositions {
+			fuel += calculateFuel(initPos, targetPos)
 		}
 
 		if fuel < minFuel {
@@ -15,4 +15,17 @@ func AlignCrabs(crabPositions []int) int {
 		}
 	}
 	return minFuel
+}
+
+func calculateFuel(pos1 int, pos2 int) int {
+	fuel := 0
+	diffBetweenPositions := int(math.Abs(float64(pos1) - float64(pos2)))
+	if diffBetweenPositions == 0 {
+		return 0
+	}
+
+	for i := 1; i <= diffBetweenPositions; i++ {
+		fuel += i
+	}
+	return fuel
 }
