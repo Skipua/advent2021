7149c8189edc8c10e6c468c98354265cecef1fd1
Volodymyr Masliy <thebestvova@gmail.com>
2021-12-19 00:09:09 +0200

    day13 binary, small x only

---
+++ day13/day13_test.go
@@ -0,0 +1,38 @@
+package day13
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	testInput(t, "example.txt", 16)
+}
+
+func TestExampleByX(t *testing.T) {
+	testInput(t, "example-by-x.txt", 17)
+}
+
+func TestInput(t *testing.T) {
+	testInput(t, "input.txt", 17)
+}
+
+func testInput(t *testing.T, filename string, want int) {
+	open, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Errorf("Couldn't read contents of input.txt")
+	}
+
+	rawInput := strings.Split(strings.TrimSpace(string(open)), "\n\n")
+	dotsRaw := rawInput[0]
+	foldsRaw := rawInput[1]
+
+	got := CountFolds(strings.Split(dotsRaw, "\n"), strings.Split(foldsRaw, "\n"))
+
+	t.Logf("Result: %v", got)
+
+	if got != want {
+		t.Errorf("Wrong! got: %v, should: %v", got, want)
+	}
+}
---
+++ day13/go.mod
@@ -0,0 +1,3 @@
+module vmas/day13
+
+go 1.17
